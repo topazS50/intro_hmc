c9e2cc6cd796d5033b748203e5008bfaf96cb866
Kenji Ogawa <ogawaknj@gmail.com>
2015-11-25 22:20:38 +0900

    added: Readme.md

--- cat_3rooms.py
+++ cat_3rooms.py
@@ -13,10 +13,12 @@ for i in range(NITER):
     pos = pos_next
     data_pos.append(pos)
 
-df = pd.DataFrame(data_pos)
-df[:100].plot()
+df = pd.DataFrame(data_pos, columns=['location of the cat'], index = None)
+df[:100].plot(style='-o', ylim=[-0.1, 2.1])
 plt.savefig('cat_3rooms.jpeg')
 
-grp = df.groupby(0)
+grp = df.groupby('location of the cat')
 df_count = grp.size()
-print df_count/df_count[0]
+
+df_ratio = pd.DataFrame(df_count.get_values()/float(df_count.get_values()[0]), columns = ['ratio of counts'], index = ['room I','room II','room III'])
+print df_ratio
